ae92fdab53937737b7dce3c20673a6550b4bfc6c
Alakbar <alakbar@M3-Pro.local>
2025-05-16 23:50:59 +0200

    Updated to post-Continuum

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const Job = ({
           {image && <img src={`logos/${image}`} alt="" />}
           <h3 className="clampText">{header}</h3>
         </div>
-        <p>
+        <p className="subtitle">
           {dateStart} - {dateEnd ?? "Now"}
         </p>
       </div>
@@ -69,15 +69,26 @@ const Job = ({
 type ProjectProps = {
   heading: string;
   link?: string;
+  period?: string;
 };
 
-const Project = ({ heading, link }: ProjectProps) => {
+const Project = ({ heading, link, period }: ProjectProps) => {
   return link ? (
-    <a href={link}>
-      <h4>{heading}</h4>
-    </a>
+    <div className="App-grid-main-content-span">
+      <div className="App-grid-main-content-span-header">
+        <a href={link}>
+          <h4 className="">{heading}</h4>
+        </a>
+      </div>
+      <p className="subtitle">{period}</p>
+    </div>
   ) : (
-    <h4>{heading}</h4>
+    <div className="App-grid-main-content-span">
+      <div className="App-grid-main-content-span-header">
+        <h4 className="clampText">{heading}</h4>
+      </div>
+      <p className="subtitle">{period}</p>
+    </div>
   );
 };
 
@@ -91,14 +102,15 @@ function App() {
             <img src="profile.jpg" alt="Head shot of Alakbar" />
             <div className="App-grid-info-summary-content">
               <h1>Alakbar</h1>
-              <a href="https://alak.bar/">https://alak.bar</a>
-              <p>me@alak.bar</p>
+              <p>hello@alak.bar</p>
+              <p>Edinburgh, UK</p>
               <p>07478447100</p>
-              <p>Edinburgh</p> {/* did you need to copy that */}
+              <a href="https://alak.bar/">https://alak.bar</a>
+              <a href="https://www.linkedin.com/in/alakbarz/">LinkedIn</a>
             </div>
           </div>
+
           <div className="App-grid-info-content">
-            <h2>Summary</h2>
             <SummarySection header="Who Me?">
               <>
                 <p>
@@ -108,46 +120,55 @@ function App() {
                 </p>
                 <br />
                 <p>
-                  My favourite hobbies are video editing/graphic design and
-                  astrophotography. I daily drive Ubuntu, Windows, and MacOS 😩 and a
-                  custom ROM on Android.
+                  My favourite hobbies are video editing, graphic design and
+                  astrophotography. I daily drive Ubuntu, Windows, MacOS 😩 and
+                  GrapheneOS on Android.
                 </p>
               </>
             </SummarySection>
             <SummarySection header="Education">
               <>
                 <p>BSc Computer Science at Heriot-Watt University (HWU).</p>
-                <p>First Class Honours.</p>
+                <p
+                  style={{
+                    fontStyle: "italic",
+                    color: "var(--subtitle)",
+                    margin: "0.25rem 0 0 0",
+                  }}
+                >
+                  First Class Honours
+                </p>
               </>
             </SummarySection>
             <SummarySection header="Programming Languages">
               <ul>
-                <li>Web development</li>
                 <li>TypeScript/JavaScript</li>
+                <li>React/Vue</li>
                 <li>Python</li>
                 <li>Go</li>
               </ul>
             </SummarySection>
             <SummarySection header="Skills">
               <ul>
-                <li><a href="https://github.com/features/actions">GitHub Actions</a> CI pipeline (DevOps)</li>
-                <li><a href="https://www.cypress.io/">Cypress</a>/<a href="https://percy.io/">Percy</a> automated visual testing</li>
-                <li>Incident management using <a href="https://incident.io/">incident.io</a> and <a href="https://newrelic.com/">New Relic</a></li>
-                <li>Automated performance testing with <a href="https://developer.chrome.com/docs/lighthouse/overview/">Lighthouse</a></li>
+                <li>CI/CD & DevOps (GitHub Actions)</li>
+                <li>Playwright/Cypress/Percy automated visual testing</li>
+                <li>Incident management (incident.io) and New Relic</li>
+                <li>
+                  Core Web Vitals (CWVs) and automated performance testing with
+                  Lighthouse
+                </li>
                 <li>Mentoring</li>
               </ul>
             </SummarySection>
             <SummarySection header="Other Skills">
               <ul>
                 <li>Photography & videography</li>
-                <li>Blender (3D)</li>
+                <li>Blender</li>
                 <li>Adobe Creative suite</li>
               </ul>
             </SummarySection>
             <SummarySection header="Miscellaneous">
               <ul>
-                <li>Photography & videography</li>
-                <li>Blender (3D)</li>
                 <li>
                   Member of <a href="https://www.mygov.scot/pvg-scheme">PVG</a>
                 </li>
@@ -176,9 +197,31 @@ function App() {
           <div className="App-grid-main-content">
             <MainSection header="Experience" />
             <Job
+              header="Continuum Industries"
+              role="Senior Software Engineer"
+              dateStart="May 2024"
+              dateEnd="April 2025"
+              image="continuum.png"
+            >
+              <>
+                <p>Still to fill in!</p>
+
+                <Project
+                  heading="Automated Regression Testing with Playwright"
+                  period="2024"
+                />
+                <p>Still to fill in!</p>
+
+                <Project heading="TypeScript Migration" period="2024" />
+                <p>Still to fill in!</p>
+              </>
+            </Job>
+
+            <Job
               header="Skyscanner"
-              role="Full stack software engineer"
+              role="Software Engineer"
               dateStart="Nov 2021"
+              dateEnd="May 2024"
               image="skyscanner.png"
             >
               <>
@@ -187,7 +230,7 @@ function App() {
                   travellers, with an emphasis on performance and accessibility.
                 </p>
 
-                <Project heading="Save To List • 2023" />
+                <Project heading="Save To List" period="2023" />
                 <p>
                   Led Save To List work-stream on web, allowing travellers to
                   save flights to their profile, increasing revenue by £1.2
@@ -195,16 +238,17 @@ function App() {
                 </p>
 
                 <Project
-                  heading="Presenting at Lean Agile Scotland • 2022"
+                  heading="Presenting at Lean Agile Scotland"
+                  period="October 2022"
                   link="https://leanagile.scot/programme/why-agile-still-hard-2022"
                 />
                 <p>
                   I wrote and presented a talk on "Why is agile still hard in
-                  2022?" with Ramsay Ashby at Lean Agile Scotland, bringing in
-                  two new employees.
+                  2022?" with Ramsay at Lean Agile Scotland, bringing in two new
+                  employees.
                 </p>
 
-                <Project heading="Homepage Consolidation • 2022" />
+                <Project heading="Homepage Consolidation" period="2022" />
                 <p>
                   Implementing redesign of new homepage and switching
                   microservice/microsite.
@@ -221,7 +265,8 @@ function App() {
             >
               <>
                 <p>
-                  Extracurriclar university project with over 100 students from HWU taking on the international{" "}
+                  Extracurriclar university project with over 100 students from
+                  HWU taking on the international{" "}
                   <a href="https://www.solardecathlon.gov/international-middle-east.html">
                     SDME
                   </a>{" "}
@@ -292,7 +337,8 @@ function App() {
             <MainSection header="Personal" />
             <br />
             <Project
-              heading="Personal website alak.bar • Summer 2020"
+              heading="Personal website alak.bar"
+              period="Summer 2020"
               link="https://alak.bar/"
             />
             <p>
@@ -308,9 +354,9 @@ function App() {
             />
             <p>
               I use <a href="https://unraid.net/">Unraid</a> to host my Plex
-              server (legit), <a href="https://filerun.com/">Filerun</a> (Google
-              Drive replacement), virtual machines, Wireguard VPN, reverse
-              proxies, and various other Docker containers.
+              server (all legit 😇), <a href="https://filerun.com/">Filerun</a>{" "}
+              (Google Drive replacement), virtual machines, Wireguard VPN,
+              reverse proxies, and various other Docker containers.
             </p>
 
             <MainSection header="Awards" />
